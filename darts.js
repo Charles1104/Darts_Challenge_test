77a8b4ce6d5b6e90c31f35a04d8cbe001a71398d
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-30 18:36:28 -1000

    Fives tests added

--- darts.js
+++ darts.js
@@ -1,13 +1,24 @@
-function scoreThrows(arr) {
+/*jshint esversion: 6 */
+
+module.exports = (arr) => {
 
   var sum = 0;
 
-  for (var i=0; i < arr.length; i++)
-    if (arr.length === 0){
+  if (arr.length === 0){
       return -1;
     }
 
-    else if (arr[i]>10){
+  if (!(Array.isArray(arr))){
+      throw "Pass an array !!!";
+    }
+
+  for (var i=0; i < arr.length; i++){
+
+    if (typeof(arr[i]) !== "number"){
+      throw "Pass numbers only !!!";
+    }
+
+    else if (arr[i]>10 || arr[i] < 1){
       sum +=0;
     }
     else if (arr[i]<=10 && arr[i]>=5){
@@ -16,8 +27,9 @@ function scoreThrows(arr) {
     else if (arr[i]<5){
       sum +=10;
     }
+  }
 
-    return sum;
-}
+  return sum;
 
-console.log(scoreThrows([1, 5, 11]));
+};
+
---
+++ test/darts.spec.js
@@ -0,0 +1,36 @@
+/*jshint esversion: 6 */
+const chai = require('chai');
+const expect = chai.expect;
+
+const darts = require('../darts.js');
+
+describe('throw function', () => {
+
+  it('should be function', () => {
+    expect(darts).to.be.a('function');
+  });
+
+  it('should return the score of certain values of input', () => {
+    expect(darts([1, 5, 11])).to.be.equal(15);
+    expect(darts([15, 20, 30])).to.be.equal(0);
+    expect(darts([6, 8, 12, 20])).to.be.equal(10);
+    expect(darts([1, 2, 1, 4, 4, 2])).to.be.equal(60);
+    expect(darts([1,7,9,14,5])).to.be.equal(25);
+  });
+
+  it('should be an array of length 1 or more', () => {
+    expect(darts([])).to.be.equal(-1);
+  });
+
+  it('should include only numbers', () => {
+    expect(darts.bind(null,["a",5])).to.throw("Pass numbers only !!!");
+    expect(darts.bind(null,[3,"b",4])).to.throw("Pass numbers only !!!");
+    expect(darts.bind(null,[3,16,4,"d"])).to.throw("Pass numbers only !!!");
+  });
+
+  it('The argument should be an array', () => {
+    expect(darts.bind(null,"bonjour")).to.throw("Pass an array !!!");
+    expect(darts.bind(null,5,4)).to.throw("Pass an array !!!");
+  });
+
+});
